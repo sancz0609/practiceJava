2ec46f98135e2e3b9d211c9013b5d0f43b7197b5
sanchit <sanchitbathla0609@gmail.com>
2018-02-16 08:25:26 +0530

    add all

--- 21.java
+++ 21.java
@@ -11,6 +11,17 @@ class B extends A{
   @Override
   public void show(){
     System.out.println("in B");       //B will override method in class A
+
+    public void config(){
+      System.out.prinln("config");
+    }
+  }
+}
+
+class C extends A{
+  @Override
+  public void show(){
+    System.out.println("in C");       //C will override method in class A
   }
 }
 
@@ -18,5 +29,6 @@ class DynamicMethodDispatch{
   public static void main(String args[]){
     A obj1 = new B();   //reference of A and object of B
     obj1.show();
+    //obj1.config();
   }
 }
